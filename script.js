c1abfe9fd8aaa22a32d8af1ef368eb1b4d751edb
G4EVA-dev <tanzeglenn@gmail.com>
2023-12-18 08:45:52 +0100

    style button

--- script.js
+++ script.js
@@ -1,25 +1,30 @@
 let currentQuoteIndex = 0;
 let currentAuthorIndex = 0;
 
+// Fetch quotes from the API and display the initial quote
+fetchQuotes();
+
 function fetchQuotes() {
   fetch("https://type.fit/api/quotes")
-  .then((res) => res.json())
-  .then((data) => {
-    displayQuote(data[currentQuoteIndex], data[currentAuthorIndex]);
+    .then((res) => res.json())
+    .then((data) => {
+      displayQuote(data[currentQuoteIndex], data[currentAuthorIndex]);
 
-    currentQuoteIndex = (currentQuoteIndex + 1) % data.length;
-    currentAuthorIndex = (currentAuthorIndex + 1) % data.length;
-  })
-  // .catch((error) => {
-  //   const errorMessage = document.getElementById('error');
-  //   errorMessage.innerHTML = 'An Error Occured. check Internet Connection'
-  // });
+      currentQuoteIndex = (currentQuoteIndex + 1) % data.length;
+      currentAuthorIndex = (currentAuthorIndex + 1) % data.length;
+    })
+    .catch((error) => {
+      console.error("Error fetching quotes:", error);
+    });
 }
-
 
 const displayQuote = (quote, author) => {
   const quoteText = document.querySelector("#quote");
   const authorText = document.querySelector("#quoteAuthor");
+
+  // Clear existing content
+  quoteText.innerHTML = "";
+  authorText.innerHTML = "";
 
   const quoteElement = document.createElement("h3");
   const authorElement = document.createElement("p");
